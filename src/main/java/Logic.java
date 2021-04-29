1fa13a50439616f708e44218b5920e48abbbfae9
Diana <basss_1993@list.ru>
2021-04-29 23:15:51 +0500

    field creation
    delete class field
    
    creation class logic
    
    creation field{
    Playing field
    }
    
    creation void PrintField(){
    Displaying the playing field on the screen
    }

---
+++ src/main/java/Logic.java
@@ -0,0 +1,19 @@
+public class Logic {
+    public char [][] field =
+            {{'+','+','+'},
+             {'+','+','+'},
+             {'+','+','+'}};// Field creation
+
+    public void PrintField() {
+        for (int i = 0; i < 3; i++){
+            for (int j = 0; j < 3; j++){
+                System.out.print(field[i][j]);
+            }
+        }
+    }
+    public static void CompRondom(){
+
+
+    }
+
+}
