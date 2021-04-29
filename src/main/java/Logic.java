2bb7a4bef5e65a7acd95022a9e19a28689a02571
Diana <basss_1993@list.ru>
2021-04-29 23:32:00 +0500

    field creation
    void HumanMove(){
    
    Human stroke
    
    }

--- src/main/java/Logic.java
+++ src/main/java/Logic.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Logic {
     public char [][] field =
             {{'+','+','+'},
@@ -10,7 +14,24 @@ public class Logic {
                 System.out.print(field[i][j]);
             }
         }
-    }
+    } //Displaying the playing field on the screen
+
+    public void HumanMove() throws IOException {
+        int x = -1, y = -1;
+        System.out.println("Enter y (1..3):");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        x = Integer.parseInt(br.readLine())-1;
+        System.out.println("Enter x (1..3)");
+        y = Integer.parseInt(br.readLine())-1;
+        while (field[x][y] == '0' || field[x][y] == 'X' || x < 0 || x > 2 || y < 0 || y > 2) {
+            System.out.println("Enter y: ");
+            x = Integer.parseInt(br.readLine())-1;
+            System.out.println("Enter x: ");
+            y = Integer.parseInt(br.readLine())-1;
+        }
+        field[x][y] = 'X';
+    } // Human stroke
+
     public static void CompRondom(){
 
 
